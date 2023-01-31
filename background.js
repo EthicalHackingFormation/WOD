1c555a5b44db98c46d61120569e158dffe5d8366
EthicalHackingFormation <44448220+EthicalHackingFormation@users.noreply.github.com>
2023-01-31 21:45:10 +0300

    FinalCommit
    Sending message and wait from fetch ApiCall Json

--- background.js
+++ background.js
@@ -6,11 +6,25 @@ chrome.runtime.onMessage.addListener((msg,sender,response) =>{
       const apiKey ="OUR-API-KEY";
       const dateStr = new Date().toISOString().slice(0,10);
       const apiCall='https://api.wordnik.com/v4/words.json/wordOfTheDay?date='+dateStr+'&api_key='+apiKey;
-    //Wait for response
-    //send the response
-        
-        //Send response
-        response({api: apiCall, date: dateStr});
+   
+     fetch(apiCall).then(function(res){
+          //Wait for response
+          if(res.status !== 200){
+              response({word:"Error",desc: "There was a problem loading the word of the day"});
+              return;
+          }
+
+          res.json().then(function(data){
+                //send response
+                response({word: data.word, desc: data.note});
+          });
+     }).catch(function(err){
+        response({word: "Error", desc: "There was a problem loading the word of the day"});
+     });
+    
+     return true;   
+       
+       
      }
 
 
--- script.js
+++ script.js
@@ -4,7 +4,9 @@ chrome.runtime.sendMessage({name: "fetchWords"}, (response) =>{
     
     
      
-     console.log(response);
+     //console.log(response);
+     document.querySelector('h1').innerHTML=response.word;
+     document.querySelector('p').innerHTML=response.desc;
    
      
 });
