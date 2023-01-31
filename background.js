7cfbf79f8e7abe9320d73edb3d901e43b74a3e53
EthicalHackingFormation <44448220+EthicalHackingFormation@users.noreply.github.com>
2023-01-31 19:36:48 +0300

    ChromeRuntime
    Background and script runtime

---
+++ background.js
@@ -0,0 +1,10 @@
+//Listen for messages
+chrome.runtime.onMessage.addListener((msg,sender,response) =>{
+   
+     if (msg.name =="message"){
+        //Send response
+        response({text: "This is a response...."});
+     }
+
+
+});
